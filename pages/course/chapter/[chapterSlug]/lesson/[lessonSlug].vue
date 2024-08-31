f4364525c6801758cd241ef6246f962a3275600f
jwansoo <jwansoo5197@gmail.com>
2024-08-31 14:20:45 +0900

    master-2

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -5,26 +5,30 @@
     </p>
     <h2 class="my-0">{{ lesson.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
-      <a
+      <NuxtLink
         v-if="lesson.sourceUrl"
         class="font-normal text-md text-gray-500"
-        :href="lesson.sourceUrl"
+        :to="lesson.sourceUrl"
       >
         Download Source Code
-      </a>
-      <a
+      </NuxtLink>
+      <NuxtLink
         v-if="lesson.downloadUrl"
         class="font-normal text-md text-gray-500"
-        :href="lesson.downloadUrl"
+        :to="lesson.downloadUrl"
       >
         Download Video
-      </a>
+      </NuxtLink>
     </div>
     <VideoPlayer 
     v-if="lesson.videoId"
     :video-id="lesson.videoId"
     />
     <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
 </template>
 <script setup>
@@ -40,4 +44,36 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`;
+});
+useHead({
+  title,
+});
+const progress = useLocalStorage('progress',[]);
+const isLessonComplete = computed(() => {
+  if (!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
+  if (
+    !progress.value[chapter.value.number - 1][
+      lesson.value.number - 1
+    ]
+  ) {
+    return false;
+  }
+  return progress.value[chapter.value.number - 1][
+    lesson.value.number - 1
+  ];
+});
+const toggleComplete = () => {
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
+  progress.value[chapter.value.number - 1][
+    lesson.value.number - 1
+  ] = !isLessonComplete.value;
+};
 </script>
+
+
